222b1de6c35dfa02bbc272575184e322f0453939
Xiaowen Lin <xlin@ilinois.edu>
2014-11-09 16:53:36 -0400

    add haml and debugger to Gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatos::Application.config.secret_token = 'a5d035c381659caf2a037165feaa89d160d9655b37bbd2efa82b8ab3e0b81ce4ccf7125a2b5d4f0ed610c13708d5f2b14959372078d6e3279aa43018e66fc9d6'
