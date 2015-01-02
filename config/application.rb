40b4f7e00280c6d41e82ee481d7f01531b9bafe4
xiaowenlin89 <xiaowenlin89@gmail.com>
2015-01-02 17:03:43 -0400

    add movies.css to precompile

--- config/application.rb
+++ config/application.rb
@@ -63,7 +63,7 @@ module Myrottenpotatos
     config.assets.enabled = true
     
     config.assets.initialize_on_precompile = false
-
+    config.assets.precompile += ['movies.css']
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
